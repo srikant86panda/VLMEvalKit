40c68afb7751f47c66d11acfd2bf04daa45574d5
hiteshlp <hiteshlp@octane-phx-bm-gpu-b4-8-ad3-015.compute.sdgdevvcn.oraclevcn.com>
2025-02-01 03:01:02 +0000

    fixing generic scripts

--- amit_generic.sh
+++ amit_generic.sh
@@ -36,24 +36,24 @@ DATASETS=(
 for DATA in "${DATASETS[@]}"; do
     echo "Running with dataset: $DATA and model: $MODEL"
 
-    python run.py --data "${DATA}" \
-      "${DATA}_grid_2x1_row1_col1" \
-      "${DATA}_grid_2x1_row2_col1" \
-      "${DATA}_grid_1x2_row1_col1" \
-      "${DATA}_grid_1x2_row1_col2" \
-      "${DATA}_grid_2x2_row1_col1" \
-      "${DATA}_grid_2x2_row1_col2" \
-      "${DATA}_grid_2x2_row2_col1" \
-      "${DATA}_grid_2x2_row2_col2" \
-      "${DATA}_grid_3x3_row1_col1" \
-      "${DATA}_grid_3x3_row1_col2" \
-      "${DATA}_grid_3x3_row1_col3" \
-      "${DATA}_grid_3x3_row2_col1" \
-      "${DATA}_grid_3x3_row2_col2" \
-      "${DATA}_grid_3x3_row2_col3" \
-      "${DATA}_grid_3x3_row3_col1" \
-      "${DATA}_grid_3x3_row3_col2" \
-      "${DATA}_grid_3x3_row3_col3" \
-      --model "${MODEL}" \
+    python run.py --data ${DATA} \
+      ${DATA}_grid_2x1_row1_col1 \
+      ${DATA}_grid_2x1_row2_col1 \
+      ${DATA}_grid_1x2_row1_col1 \
+      ${DATA}_grid_1x2_row1_col2 \
+      ${DATA}_grid_2x2_row1_col1 \
+      ${DATA}_grid_2x2_row1_col2 \
+      ${DATA}_grid_2x2_row2_col1 \
+      ${DATA}_grid_2x2_row2_col2 \
+      ${DATA}_grid_3x3_row1_col1 \
+      ${DATA}_grid_3x3_row1_col2 \
+      ${DATA}_grid_3x3_row1_col3 \
+      ${DATA}_grid_3x3_row2_col1 \
+      ${DATA}_grid_3x3_row2_col2 \
+      ${DATA}_grid_3x3_row2_col3 \
+      ${DATA}_grid_3x3_row3_col1 \
+      ${DATA}_grid_3x3_row3_col2 \
+      ${DATA}_grid_3x3_row3_col3 \
+      --model ${MODEL} \
       --verbose
 done
